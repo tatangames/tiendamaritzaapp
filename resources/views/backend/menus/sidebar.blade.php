9b38050ab9373dda0ed172bc64fcd34b1cf21b7b
tatangames <54828743+tatangames@users.noreply.github.com>
2023-08-01 20:35:34 -0600

    ajuste peque

--- resources/views/backend/menus/sidebar.blade.php
+++ resources/views/backend/menus/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="#" class="brand-link">
         <img src="{{ asset('images/logonaranja.png') }}" alt="Logo" class="brand-image img-circle elevation-3" >
-        <span class="brand-text font-weight" style="color: white">PANEL DE CONTROL</span>
+        <span class="brand-text font-weight" style="color: white">PANEL</span>
     </a>
 
     <div class="sidebar">
